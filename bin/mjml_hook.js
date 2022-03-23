ec5a9fafc35dc4c2900976afb6b1a9cde47071dc
Matt Kramer <mkramer@anaconda.com>
2022-03-23 15:19:58 -0500

    Change output to only log to console if error code not zero

--- bin/mjml_hook.js
+++ bin/mjml_hook.js
@@ -7,6 +7,7 @@
 
 import {spawn} from 'child_process';
 
+
 // The first two arguments are node and then mjml_hook
 const templates = process.argv.slice(2);
 
@@ -19,14 +20,19 @@ for (const template of templates) {
   // https://stackoverflow.com/a/16099450
   const prc = spawn('mjml', ['-o', output, template]);
 
+  // Capture stdout
+  let lines = [];
   prc.stdout.setEncoding('utf8');
   prc.stdout.on('data', function(data) {
     const str = data.toString();
-    const lines = str.split(/(\r?\n)/g);
-    console.log(lines.join(''));
+    lines = str.split(/(\r?\n)/g);
   });
 
+  // Log to console if any non-zero error codes
   prc.on('close', function(code) {
-    console.log('process exit code ' + code);
+    if (code !== 0) {
+      console.log('process exit code ' + code);
+      console.log(lines.join(''));
+    }
   });
 }
